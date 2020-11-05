9e8775a668f7e2a067ba9f0a153e4a06d1dbc3e0
Genis Bardales <genis96@yahoo.com>
2020-11-05 01:11:07 -0500

    began the javascript and pseudo code

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const form = document.querySelector('#form').onchange = () => {
+    let billAmt = document.querySelector('#bill-amt').value;
+    let percentage = document.querySelector('#choose-percentage').value;
+    let tipAmt = document.querySelector('#tip-amt');
+    let totalBill = document.querySelector('#total-bill');
+
+    if(billAmt === '' || percentage === 0) {
+        alert('Required: Enter Value');
+        return;
+    }
+
+    tipAmt = billAm / percentage;
+
+}
 
 
 
@@ -5,8 +19,19 @@
 
 
 
+/** PSEUDO CODE
+ - Grab the html elements 
+ - The number you input inside billAmt must link to the percentage chosen through the <option> value tags in html. It must collect the input inside this input tag. 
+ - REQUIRED to enter number or else send alert
+ - It must also do math: billAmt number * percentage = tipAmt 
+ - the total math score must then link and display to the tipAmt 
+ - The tipAmt must now be added to the billAmt to equal totalBill 
+ - the math: billAmt + tipAmt = totalBill and it must display in the totalBill input. 
 
 
+     // let billAmt = Number(document.querySelector('#bill-amt').value);
+
+ */
 
 
 
